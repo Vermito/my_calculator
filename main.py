b7275929271df13a01ae5aca1a7f80e53d28d75a
akhmad <akuev@mail.ru>
2026-04-29 20:47:10 +0300

    Создал class Math с методом сложения

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ print(subtract(5, 5))
 def multiply(a, b):
     print(a * b)
 
+
+class Math:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return add(self.a, self.b)
+
+obj = Math(5, 5)
+print(obj.add())
